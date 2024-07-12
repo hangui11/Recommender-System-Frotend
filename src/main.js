18baaa31d54b3a7500af418362209ecffeb92951
hangui11 <hangyao2002@gmail.com>
2024-07-12 13:39:13 +0200

    update main

--- src/main.js
+++ src/main.js
@@ -21,8 +21,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  // history: createWebHistory(process.env.BASE_URL), // remote
-  history: createWebHistory(), // local
+  history: createWebHistory(process.env.BASE_URL), // remote
+  // history: createWebHistory(), // local
   routes
 })
 
